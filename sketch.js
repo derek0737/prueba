b70acce8fb6ed112c29eba4a09de91db32f086d8
derek0737 <101231297+derek0737@users.noreply.github.com>
2022-03-08 20:24:27 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var caja;
+console.log("holaaaa");
+var trex ,trex_running;
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(400,400)
+  
+  caja=createSprite(200,200,30,30);
+ 
+}
+
+function draw(){
+  background("blue")
+  drawSprites();
+
+}
